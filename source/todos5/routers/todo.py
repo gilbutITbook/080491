b33c1f378539c302ad75777715a06aa83b9dd4b5
길벗 IT전문서/입문서 <6995518+gilbutITbook@users.noreply.github.com>
2026-04-29 09:29:17 +0900

    Update todo.py

--- source/todos5/routers/todo.py
+++ source/todos5/routers/todo.py
@@ -39,7 +39,7 @@ def get_todo_handler(todo_id: int):
             return todo
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail="Todo not found",
+            detail="Todo not found"
         )
     finally:
         session.close()
@@ -83,7 +83,7 @@ def update_todo_handler(todo_id: int, body: TodoUpdateRequest):
             return todo
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail="Todo not found",
+            detail="Todo not found"
         )
     finally:
         session.close()
